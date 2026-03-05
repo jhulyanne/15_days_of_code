7cc2d3872eec9364f8bf5d771a10cc78f204f523
jhulyanne <jhulyannedeoliveira@gmail.com>
2026-03-05 05:57:12 -0300

    first commit

---
+++ day_01/script.js
@@ -0,0 +1,26 @@
+/*
+Escreva um programa que recebe um número inteiro e mostra:
+- se é par ou ímpar
+- se é positivo ou negativo
+- quantos dígitos ele possui
+*/
+
+function oddOrEven (num) {
+    if(num % 2 == 0) {
+        return `${num} é par`
+    } else if (num % 2 == 1){
+        return `${num} é ímpar`
+    } else {
+        return `${num} não é um número, tente novamente`
+    }
+}
+
+function posOrNeg(num) {
+    if (num >= 0) {
+        return `${num} é positivo`
+    } else if (num < 0) {
+        return `${num} é negativo`
+    } else {
+        return `${num} não é um número, tente novamente`
+    }
+}
