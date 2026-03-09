c3fdb0674720b3a3101f2eb328793efa3d4fa4dc
jhulyanne <jhulyannedeoliveira@gmail.com>
2026-03-09 17:46:31 -0300

    initial attempt at logic

---
+++ day_04/script.js
@@ -0,0 +1,9 @@
+var newList = document.getElementById("newList").value;
+var finalList = document.getElementById("finalList");
+ 
+// newList vai ser inserido em finalList com um tratamento para transformar a string em um array
+
+function itemsList (){
+    newList.split(', ')
+    return 
+} 
